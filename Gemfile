dca083a4010bf3dfbc0384ec30737e3b9b9823df
Umar Gulraiz <umar.gulraiz1@gmail.com>
2021-06-04 01:21:43 +0500

    setup for user and admin

--- Gemfile
+++ Gemfile
@@ -21,6 +21,13 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+# For Authentication
+gem 'devise'
+# haml for rails
+gem "haml-rails", "~> 2.0"
+# PDF generation
+gem 'wicked_pdf'
+gem 'wkhtmltopdf-binary'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
+
+  root to: "user/dashboard#index"
+
+  get '/admin', to: "admin#index"
+
+  namespace :admin do
+    resources :dashboard, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
